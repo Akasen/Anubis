185a8ac4f3243cd9c113bcec85510b56284f745d
Ryan Hammett <rhammett@ycp.edu>
2016-07-15 14:28:04 -0400

    Added preamble

--- bot.go
+++ bot.go
@@ -1,3 +1,24 @@
+/*
+
+Anubis: A Twitch IRC Bot
+Copyright (C) 2016  Akasen, Ryan Hammett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Main Bot Go file
 package main
 
 import (
